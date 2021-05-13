a842bd005dddbf11420c8c500cac41675962c4d7
Laraib07 <A.Laraib@protonmail.com>
2021-05-13 13:03:11 +0530

    synced with upstream and applied desired changes

--- config.js
+++ config.js
@@ -1,50 +1,83 @@
+<<<<<<< HEAD
 // Username displayed in greeting.
-var userName = "Faizan";
+var userName = "LaraiB";
+=======
+// Username to be displayed in greeting
+var userName = "Laraib";
+>>>>>>> 7cb880e99ff7b2d9b9149cc58f7a4bcc9787ef69
 
 // Default Searchengine
 // Available: Google DuckDuckGo Bing Yahoo
-var searchEngine = "Yahoo";
+var searchEngine = "DuckDuckGo";
 
+<<<<<<< HEAD
 // Cards
 var cards = [
 	{
-		name: "General",
-		bookmarks: {
-			"google": "aaa",
-			"dirive": "aaa",
-			"maps": "aaa",
-			"photos": "aaa",
-			"mega": "aaa"
-		}
-	},
-	{
-		name: "Tech",
-		bookmarks: {
-			"github": "aaa",
-			"freenode": "aaa",
-			"stackoverflow": "aaa",
-			"github": "aaa",
-			"freenode": "aaa"
-		}
-	},
-	{
 		name: "Work",
 		bookmarks: {
-			"subreddit": "aaa",
-			"youtube": "aaa",
-			"spotify": "aaa",
-			"amazon": "aaa",
-			"dummysite": "aaa"
+			"github": "https://github.com",
+			"leetcode": "https://leetcode.com"
+		}
+	},
+	{
+		name: "Doc",
+		bookmarks: {
+			"android": "https://developer.android.com",
+			"kotlin": "https://www.programiz.com/kotlin-programming"
 		}
 	},
 	{
 		name: "Social",
 		bookmarks: {
-			"youtube": "https://www.youtube.com",
-			"twitter": "https://twitter.com",
-			"imgur": "https://imgur.com/",
-			"facebook": "https://www.facebook.com",
-			"reddit": "https://www.reddit.com"
+			"r/linux": "https://teddit.net/r/linux/",
+			"reddit": "https://teddit.net",
+			"youtube": "https://youtube.com",
+			"phoronix": "https://www.phoronix.com"
+		}
+	},
+	{
+		name: "Misc",
+		bookmarks: {
+			"merriam-webster": "https://www.merriam-webster.com/",
+			"translate": "https://translate.google.co.in"
 		}
 	}
+=======
+
+// Add link or remove
+var links_var = [
+    [
+
+        // Work
+        ['github', 'https://github.com'],
+        ['stackoverflow', 'https://stackoverflow.com'],
+
+
+    ],
+    [
+
+        // Media
+        ['youtube', 'https://www.youtube.com'],
+        ['reddit', 'https://teddit.net/'],
+
+
+    ],
+    [
+
+        // Doc
+        ['kotlin', 'https://www.programiz.com/kotlin-programming'],
+        ['android', 'https://developer.android.com'],
+       // ['', ''],
+
+
+    ],
+    [
+
+        // Misc
+        ['translate', 'https://translate.google.co.in/'],
+
+
+    ],
+>>>>>>> 7cb880e99ff7b2d9b9149cc58f7a4bcc9787ef69
 ]
